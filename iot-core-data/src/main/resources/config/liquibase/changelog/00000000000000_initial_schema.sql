0b7931dff5d81eab8c4099580c1ab2aa79ab26eb
Mohammadali Azani <aliazn2013@yahoo.com>
2022-03-28 02:15:14 +0430

    add properties for bulk insert, disable liquibase, some other properties for optimization(hikari).

--- iot-core-data/src/main/resources/config/liquibase/changelog/00000000000000_initial_schema.sql
+++ iot-core-data/src/main/resources/config/liquibase/changelog/00000000000000_initial_schema.sql
@@ -1,36 +1,23 @@
 -- liquibase formatted sql
 
--- changeset ali: 1
-CREATE TABLE IF NOT EXISTS device
-(
-    id          BIGINT AUTO_INCREMENT NOT NULL,
-    mac_address VARCHAR(255)          NULL,
-    CONSTRAINT PK_DEVICE PRIMARY KEY (id)
-);
+-- changeset mohammadali:1648417070412-1
+CREATE TABLE device (id BIGINT AUTO_INCREMENT NOT NULL, mac_address VARCHAR(255) NULL, CONSTRAINT PK_DEVICE PRIMARY KEY (id));
 
--- changeset ali: 2
-CREATE TABLE IF NOT EXISTS topic
-(
-    id   BIGINT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255)          NULL,
-    CONSTRAINT PK_TOPIC PRIMARY KEY (id)
-);
+-- changeset mohammadali:1648417070412-2
+CREATE TABLE message (id BIGINT AUTO_INCREMENT NOT NULL, content VARCHAR(255) NULL, created_time_stamp datetime NULL, message_type INT NULL, device_id BIGINT NULL, topic_id BIGINT NULL, CONSTRAINT PK_MESSAGE PRIMARY KEY (id));
 
--- changeset ali: 3
-CREATE TABLE IF NOT EXISTS message
-(
-    id                 BIGINT AUTO_INCREMENT NOT NULL,
-    content            VARCHAR(255)          NULL,
-    created_time_stamp datetime              NULL,
-    message_type       INT                   NULL,
-    device_id          BIGINT                NULL,
-    topic_id           BIGINT                NULL,
-    CONSTRAINT PK_MESSAGE PRIMARY KEY (id),
-    UNIQUE (device_id),
-    UNIQUE (topic_id)
-);
-ALTER TABLE message
-    ADD CONSTRAINT FKkjoquerqtrcvqgnxlkfn7iwv0 FOREIGN KEY (topic_id) REFERENCES topic (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
-ALTER TABLE message
-    ADD CONSTRAINT FKml7ww4agpbfyn5eiqq5o9vte6 FOREIGN KEY (device_id) REFERENCES device (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+-- changeset mohammadali:1648417070412-3
+CREATE TABLE topic (id BIGINT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NULL, CONSTRAINT PK_TOPIC PRIMARY KEY (id));
+
+-- changeset mohammadali:1648417070412-4
+CREATE INDEX FKkjoquerqtrcvqgnxlkfn7iwv0 ON message(topic_id);
+
+-- changeset mohammadali:1648417070412-5
+CREATE INDEX FKml7ww4agpbfyn5eiqq5o9vte6 ON message(device_id);
+
+-- changeset mohammadali:1648417070412-6
+ALTER TABLE message ADD CONSTRAINT FKkjoquerqtrcvqgnxlkfn7iwv0 FOREIGN KEY (topic_id) REFERENCES topic (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+-- changeset mohammadali:1648417070412-7
+ALTER TABLE message ADD CONSTRAINT FKml7ww4agpbfyn5eiqq5o9vte6 FOREIGN KEY (device_id) REFERENCES device (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
