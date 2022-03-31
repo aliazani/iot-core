ee33d33feaaa0ffd5d02f5fcf07cb92caef2a3d5
Mohammadali Azani <aliazn2013@yahoo.com>
2022-03-31 22:41:10 +0430

    add dto objects.

---
+++ iot-core-web/src/main/java/com/example/iotcore/security/dto/PasswordChangeDTO.java
@@ -0,0 +1,18 @@
+package com.example.iotcore.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * A DTO representing a password change required data - current and new password.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PasswordChangeDTO {
+
+    private String currentPassword;
+
+    private String newPassword;
+}
---
+++ iot-core-web/src/main/java/com/example/iotcore/security/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.example.iotcore.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * A DTO representing a user, with only the public attributes.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+
+    private String login;
+//    public UserDTO(User user) {
+//        this.id = user.getId();
+//        this.login = user.getLogin();
+//    }
+}
