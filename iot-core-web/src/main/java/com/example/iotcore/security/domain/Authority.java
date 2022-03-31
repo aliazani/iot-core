fb432ab88f82381887eae6cb370d5197f132fb37
Mohammadali Azani <aliazn2013@yahoo.com>
2022-03-31 18:09:58 +0430

    add user and authority domain objects.

---
+++ iot-core-web/src/main/java/com/example/iotcore/security/domain/Authority.java
@@ -0,0 +1,33 @@
+package com.example.iotcore.security.domain;
+
+import lombok.Data;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * An authority (a security role) used by Spring Security.
+ */
+@Data
+@Entity
+@Table(name = "authority")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Authority implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Size(max = 50)
+    @Id
+    @Column(length = 50)
+    private String name;
+}
