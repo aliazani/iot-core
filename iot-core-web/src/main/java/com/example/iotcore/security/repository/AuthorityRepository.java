760f8f4ca10cfc7f48bd0ed44d9f94b7009042d4
Mohammadali Azani <aliazn2013@yahoo.com>
2022-03-31 18:10:38 +0430

    add user and authority repositories.

---
+++ iot-core-web/src/main/java/com/example/iotcore/security/repository/AuthorityRepository.java
@@ -0,0 +1,10 @@
+package com.example.iotcore.security.repository;
+
+import com.example.iotcore.security.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
