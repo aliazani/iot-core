3e4a191ffa363673d733df9c002d327798e3d063
Mohammadali Azani <aliazn2013@yahoo.com>
2022-04-01 23:33:45 +0430

    move mail configuration to config package.

--- iot-core-web/src/main/java/com/example/iotcore/config/MailConfiguration.java
+++ iot-core-web/src/main/java/com/example/iotcore/config/MailConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.iotcore.config.property;
+package com.example.iotcore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
