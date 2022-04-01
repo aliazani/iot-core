0f4b720189160413369390da3096b21178f6ce55
Mohammadali Azani <aliazn2013@yahoo.com>
2022-04-01 20:06:23 +0430

    add repository annotation.

--- iot-core-web/src/main/java/com/example/iotcore/security/repository/AuthorityRepository.java
+++ iot-core-web/src/main/java/com/example/iotcore/security/repository/AuthorityRepository.java
@@ -2,9 +2,11 @@ package com.example.iotcore.security.repository;
 
 import com.example.iotcore.security.domain.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data JPA repository for the {@link Authority} entity.
  */
+@Repository
 public interface AuthorityRepository extends JpaRepository<Authority, String> {
 }
