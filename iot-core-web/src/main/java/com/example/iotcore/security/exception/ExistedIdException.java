059a9bc1cd0b1bc460ab2cc42645e1aee1c84097
Mohammadali Azani <aliazn2013@yahoo.com>
2022-04-02 05:53:54 +0430

    add exception handling and details for openApi.

--- iot-core-web/src/main/java/com/example/iotcore/security/exception/ExistedIdException.java
+++
@@ -1,7 +0,0 @@
-package com.example.iotcore.security.exception;
-
-public class ExistedIdException extends RuntimeException {
-    public ExistedIdException(String message) {
-        super(message);
-    }
-}
--- iot-core-web/src/main/java/com/example/iotcore/web/controller/sync/DeviceControllerSync.java
+++ iot-core-web/src/main/java/com/example/iotcore/web/controller/sync/DeviceControllerSync.java
@@ -2,33 +2,55 @@ package com.example.iotcore.web.controller.sync;
 
 import com.example.iotcore.domain.Device;
 import com.example.iotcore.dto.DeviceDTO;
+import com.example.iotcore.repository.DeviceRepository;
 import com.example.iotcore.service.sync.DeviceServiceSync;
+import com.example.iotcore.util.HeaderUtil;
+import com.example.iotcore.util.PaginationUtil;
+import com.example.iotcore.util.ResponseUtil;
+import com.example.iotcore.web.errors.BadRequestAlertException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
  * REST controller for managing {@link Device}.
  */
+@Tag(name = "Device", description = "Device Management")
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 @Profile("sync")
 public class DeviceControllerSync {
+
     private static final String ENTITY_NAME = "device";
     private final DeviceServiceSync deviceService;
+    private final DeviceRepository deviceRepository;
+    @Value("${spring.clientApp.name}")
+    private String applicationName;
 
     /**
      * {@code POST  /devices} : Create a new device.
@@ -37,14 +59,40 @@ public class DeviceControllerSync {
      * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new deviceDTO, or with status {@code 400 (Bad Request)} if the device has already an ID.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
+    @Operation(summary = "Create new device", description = "Create new device",
+            security = {@SecurityRequirement(name = "bearer-key")},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Device created successfully",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = DeviceDTO.class))}
+                            , headers = @Header(name = "Location", description = "The URL of the created device")
+                    ),
+                    @ApiResponse(responseCode = "400",
+                            description = "Bad request (A new device cannot already have an ID)",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Authentication Failure",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(required = true,
+            content = @Content(examples = {@ExampleObject(name = "DeviceDTO",
+                    value = "{\"macAddress\":\"string\"}")}))
     @PostMapping("/devices")
     public ResponseEntity<DeviceDTO> createDevice(@Valid @RequestBody DeviceDTO deviceDTO) throws URISyntaxException {
         log.debug("REST request to save Device : {}", deviceDTO);
+
+        if (deviceDTO.getId() != null)
+            throw new BadRequestAlertException("A new %s cannot already have an ID".formatted(ENTITY_NAME)
+                    , ENTITY_NAME, "idexists");
 
         DeviceDTO result = deviceService.save(deviceDTO);
 
         return ResponseEntity
                 .created(new URI("/api/devices/" + result.getId()))
+                .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME,
+                        result.getId().toString()))
                 .body(result);
     }
 
@@ -56,19 +104,35 @@ public class DeviceControllerSync {
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated deviceDTO,
      * or with status {@code 400 (Bad Request)} if the deviceDTO is not valid,
      * or with status {@code 500 (Internal Server Error)} if the deviceDTO couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
+    @Operation(summary = "Update a device", description = "Update a device",
+            security = {@SecurityRequirement(name = "bearer-key")},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Device updated successfully",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = DeviceDTO.class))}
+                    ),
+                    @ApiResponse(responseCode = "400",
+                            description = "Bad request (Device is not valid)",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Authentication Failure",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @PutMapping("/devices/{id}")
-    public ResponseEntity<DeviceDTO> updateDevice(
-            @PathVariable(value = "id", required = false) final Long id,
-            @Valid @RequestBody DeviceDTO deviceDTO
-    ) throws URISyntaxException {
+    public ResponseEntity<DeviceDTO> updateDevice(@PathVariable(value = "id", required = false) final Long id,
+                                                  @Valid @RequestBody DeviceDTO deviceDTO) {
         log.debug("REST request to update Device : {}, {}", id, deviceDTO);
 
+        checkIdValidity(deviceDTO, id);
         DeviceDTO result = deviceService.save(deviceDTO);
 
         return ResponseEntity
                 .ok()
+                .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME,
+                        deviceDTO.getId().toString()))
                 .body(result);
     }
 
@@ -81,18 +145,40 @@ public class DeviceControllerSync {
      * or with status {@code 400 (Bad Request)} if the deviceDTO is not valid,
      * or with status {@code 404 (Not Found)} if the deviceDTO is not found,
      * or with status {@code 500 (Internal Server Error)} if the deviceDTO couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
+    @Operation(summary = "Patch a device", description = "Patch a device",
+            security = {@SecurityRequirement(name = "bearer-key")},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Device patched successfully",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = DeviceDTO.class))}
+                    ),
+                    @ApiResponse(responseCode = "400",
+                            description = "Bad request (Device is not valid)",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Authentication Failure",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+                    @ApiResponse(responseCode = "404",
+                            description = "Not found (to patch)",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @PatchMapping(value = "/devices/{id}", consumes = {"application/json", "application/merge-patch+json"})
-    public ResponseEntity<DeviceDTO> partialUpdateDevice(
-            @PathVariable(value = "id", required = false) final Long id,
-            @NotNull @RequestBody DeviceDTO deviceDTO
-    ) throws URISyntaxException {
+    public ResponseEntity<DeviceDTO> partialUpdateDevice(@PathVariable(value = "id", required = false) final Long id,
+                                                         @NotNull @RequestBody DeviceDTO deviceDTO) {
         log.debug("REST request to partial update Device partially : {}, {}", id, deviceDTO);
 
+        checkIdValidity(deviceDTO, id);
         Optional<DeviceDTO> result = deviceService.partialUpdate(deviceDTO);
 
-        return ResponseEntity.ok().body(result.orElse(null));
+        return ResponseUtil.wrapOrNotFound(
+                result,
+                HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME,
+                        deviceDTO.getId().toString())
+        );
     }
 
     /**
@@ -101,13 +187,28 @@ public class DeviceControllerSync {
      * @param pageable the pagination information.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of devices in body.
      */
+
+    @Operation(summary = "Get all devices paged", description = "Get all devices paged",
+            security = {@SecurityRequirement(name = "bearer-key")},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "successfully retrieved a page of devices",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = DeviceDTO.class))}
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Authentication Failure",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @GetMapping("/devices")
     public ResponseEntity<List<DeviceDTO>> getAllDevices(@org.springdoc.api.annotations.ParameterObject Pageable pageable) {
         log.debug("REST request to get a page of Devices");
 
         Page<DeviceDTO> page = deviceService.findAll(pageable);
+        HttpHeaders headers =
+                PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
 
-        return ResponseEntity.ok().body(page.getContent());
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
     }
 
     /**
@@ -116,13 +217,30 @@ public class DeviceControllerSync {
      * @param id the id of the deviceDTO to retrieve.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the deviceDTO, or with status {@code 404 (Not Found)}.
      */
+
+    @Operation(summary = "Find a device by ID", description = "Find a device by ID",
+            security = {@SecurityRequirement(name = "bearer-key")},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "successfully retrieved a device",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = DeviceDTO.class))}
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Authentication Failure",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+                    @ApiResponse(responseCode = "404",
+                            description = "Device Not found",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @GetMapping("/devices/{id}")
     public ResponseEntity<DeviceDTO> getDevice(@PathVariable Long id) {
         log.debug("REST request to get Device : {}", id);
 
         Optional<DeviceDTO> deviceDTO = deviceService.findOne(id);
 
-        return ResponseEntity.ok().body(deviceDTO.orElse(null));
+        return ResponseUtil.wrapOrNotFound(deviceDTO);
     }
 
     /**
@@ -131,6 +249,19 @@ public class DeviceControllerSync {
      * @param id the id of the deviceDTO to delete.
      * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
      */
+
+    @Operation(summary = "Delete a device", description = "Delete a device",
+            security = {@SecurityRequirement(name = "bearer-key")},
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Device successfully deleted",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = DeviceDTO.class))}
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Authentication Failure",
+                            content = @Content(schema = @Schema(hidden = true))
+                    )
+            }
+    )
     @DeleteMapping("/devices/{id}")
     public ResponseEntity<Void> deleteDevice(@PathVariable Long id) {
         log.debug("REST request to delete Device : {}", id);
@@ -139,6 +270,18 @@ public class DeviceControllerSync {
 
         return ResponseEntity
                 .noContent()
+                .headers(HeaderUtil.createEntityDeletionAlert(applicationName, true,
+                        ENTITY_NAME, id.toString()))
                 .build();
+    }
+
+    private void checkIdValidity(DeviceDTO deviceDTO, Long id) {
+        if (deviceDTO.getId() == null)
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        if (!Objects.equals(id, deviceDTO.getId()))
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+
+        if (!deviceRepository.existsById(id))
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
     }
 }
