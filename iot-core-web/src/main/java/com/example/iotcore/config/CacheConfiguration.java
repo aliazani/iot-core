a46d99a035ad2c0acbb123d52ec56d9743a26289
Mohammadali Azani <aliazn2013@yahoo.com>
2022-04-04 00:48:36 +0430

    add cache config, reformat jackson config.

--- iot-core-web/src/main/java/com/example/iotcore/config/CacheConfiguration.java
+++ iot-core-web/src/main/java/com/example/iotcore/config/CacheConfiguration.java
@@ -5,7 +5,9 @@ import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.jsr107.Eh107Configuration;
+import org.hibernate.cache.jcache.ConfigSettings;
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,20 +32,24 @@ public class CacheConfiguration {
     }
 
     @Bean
+    public HibernatePropertiesCustomizer hibernatePropertiesCustomizer(javax.cache.CacheManager cacheManager) {
+        return hibernateProperties -> hibernateProperties.put(ConfigSettings.CACHE_MANAGER, cacheManager);
+    }
+
+    @Bean
     public JCacheManagerCustomizer cacheManagerCustomizer() {
         return cm -> {
             createCache(cm, com.example.iotcore.security.repository.UserRepository.USERS_BY_LOGIN_CACHE);
             createCache(cm, com.example.iotcore.security.repository.UserRepository.USERS_BY_EMAIL_CACHE);
-            // jhipster-needle-ehcache-add-entry
+            createCache(cm, com.example.iotcore.security.domain.User.class.getName());
+            createCache(cm, com.example.iotcore.security.domain.Authority.class.getName());
+            createCache(cm, com.example.iotcore.security.domain.User.class.getName() + ".authorities");
         };
     }
 
     private void createCache(javax.cache.CacheManager cm, String cacheName) {
         javax.cache.Cache<Object, Object> cache = cm.getCache(cacheName);
-        if (cache != null) {
-            cache.clear();
-        } else {
-            cm.createCache(cacheName, jcacheConfiguration);
-        }
+        if (cache != null) cache.clear();
+        else cm.createCache(cacheName, jcacheConfiguration);
     }
 }
