2a9af34bbbde3fdc0f24854fe27c21fdf60d016d
Mohammadali Azani <aliazn2013@yahoo.com>
2022-04-04 16:16:28 +0430

    add dto tests to data layer too.

---
+++ iot-core-data/src/test/java/com/example/iotcore/dto/DeviceDTOTest.java
@@ -0,0 +1,34 @@
+package com.example.iotcore.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DeviceDTOTest {
+    DeviceDTO deviceDTO1;
+    DeviceDTO deviceDTO2;
+
+    @BeforeEach
+    void setup() {
+        deviceDTO1 = new DeviceDTO();
+        deviceDTO2 = new DeviceDTO();
+    }
+
+    @Test
+    void equalDeviceDTOs() {
+        deviceDTO1.setId(1L);
+        deviceDTO2.setId(deviceDTO1.getId());
+
+        assertThat(deviceDTO1).isEqualTo(deviceDTO2);
+    }
+
+    @Test
+    void unequalDeviceDTOs() {
+        deviceDTO1.setId(1L);
+        deviceDTO2.setId(2L);
+        assertThat(deviceDTO1).isNotEqualTo(deviceDTO2);
+        deviceDTO1.setId(null);
+        assertThat(deviceDTO1).isNotEqualTo(deviceDTO2);
+    }
+}
---
+++ iot-core-data/src/test/java/com/example/iotcore/dto/MessageDTOTest.java
@@ -0,0 +1,34 @@
+package com.example.iotcore.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MessageDTOTest {
+    MessageDTO messageDTO1;
+    MessageDTO messageDTO2;
+
+    @BeforeEach
+    void setup() {
+        messageDTO1 = new MessageDTO();
+        messageDTO2 = new MessageDTO();
+    }
+
+    @Test
+    void equalMessageDTOs() {
+        messageDTO1.setId(1L);
+        messageDTO2.setId(messageDTO1.getId());
+
+        assertThat(messageDTO1).isEqualTo(messageDTO2);
+    }
+
+    @Test
+    void unequalMessageDTOs() {
+        messageDTO1.setId(1L);
+        messageDTO2.setId(2L);
+        assertThat(messageDTO1).isNotEqualTo(messageDTO2);
+        messageDTO1.setId(null);
+        assertThat(messageDTO1).isNotEqualTo(messageDTO2);
+    }
+}
