19959a516d667897b4359ecc64bb4cb232f4b374
Mohammadali Azani <aliazn2013@yahoo.com>
2022-04-07 01:03:43 +0430

    Integration test for UserJWTController.

---
+++ iot-core-web/src/test/java/com/example/iotcore/security/controller/UserJWTControllerTest.java
@@ -0,0 +1,62 @@
+package com.example.iotcore.security.controller;
+
+import com.example.iotcore.MySqlExtension;
+import com.example.iotcore.security.controller.vm.LoginVM;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserJWTControllerTest extends MySqlExtension {
+    private static final String ENTITY_API_URL = "/api/authenticate";
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void authorize_validCredentials() throws Exception {
+        LoginVM loginVM = new LoginVM();
+        loginVM.setUsername("admin");
+        loginVM.setPassword("admin");
+        loginVM.setRememberMe(true);
+
+        mockMvc.perform(post(ENTITY_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginVM))
+                )
+                .andExpect(status().isOk())
+                .andExpect(header().exists(HttpHeaders.AUTHORIZATION));
+    }
+
+    @Test
+    void authorize_invalidCredentials() throws Exception {
+        LoginVM loginVM = new LoginVM();
+        loginVM.setUsername("wrong");
+        loginVM.setPassword("wrong");
+        loginVM.setRememberMe(true);
+
+        mockMvc.perform(post(ENTITY_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginVM))
+                )
+                .andExpect(status().isUnauthorized());
+    }
+}
