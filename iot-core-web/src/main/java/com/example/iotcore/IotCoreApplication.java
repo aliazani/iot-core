094f63ccb40c64c1792026572d4f9455024f9c78
Mohammadali Azani <aliazn2013@yahoo.com>
2022-04-08 00:36:36 +0430

    remove slf4j from main application.

--- iot-core-web/src/main/java/com/example/iotcore/IotCoreApplication.java
+++ iot-core-web/src/main/java/com/example/iotcore/IotCoreApplication.java
@@ -1,13 +1,10 @@
 package com.example.iotcore;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class IotCoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IotCoreApplication.class, args);
     }
